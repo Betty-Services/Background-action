6943dee5fe9f6305c2d26dba5b07b1f201038293
robinvanderburght <37829379+robinvanderburght@users.noreply.github.com>
2023-07-27 13:55:11 +0200

    Update index.js
    Update with latest code version

--- functions/background-subaction/1.0/index.js
+++ functions/background-subaction/1.0/index.js
@@ -1,5 +1,36 @@
-const backgroundSubaction = ({ action: id, input }) => ({
-  result: runAction({ id, input }),
-});
+const runInBackground = async ({
+  app_identifier,
+  app_id,
+  jwt,
+  action_id,
+  variables,
+}) => {
+  const variableMap = variables.reduce((previousValue, currentValue) => {
+    previousValue[currentValue.key] = currentValue.value;
+    return previousValue;
+  }, {});
 
-export default backgroundSubaction;
+  fetch(`https://${app_identifier}.betty.app/api/runtime/${app_id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...(jwt ? { Authorization: jwt } : {}),
+    },
+    body: JSON.stringify({
+      query: `
+          mutation { 
+            action(id: "${action_id}", input: $input)
+          }
+        `,
+      variables: {
+        input: variableMap,
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export default runInBackground;
